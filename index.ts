68b16bd37cb715c51f7f4e5743fef17ea77714e1
Andrew Wittrock <andy@wittrock.us>
2026-05-17 14:16:59 -0500

    Add FieldRegistry to only allow some fields to be updated via the API

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+import { FieldRegistry } from "@core/express/util";
 import { init } from "../donation-products-plugin/migrations";
 
 export { apiConfig } from "./endpoints";
 
 export const migrations = [init];
 export const setupMigrations = [init];
+
+FieldRegistry.register("donations", ["amount", "productId", "orderId", "transactionId", "status", "note"]);
+FieldRegistry.register("products", ["isDonation", "isSetYourOwnAmount"]);
